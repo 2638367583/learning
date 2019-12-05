d6930337b5184c8df3b433cfd0a8c1b800f31bc0
leiyudaye <2638367583@qq.com>
2019-12-05 22:22:32 +0800

    [OPT] 优化c++代码结构

---
+++ c++/exam/twoSum/main.cpp
@@ -0,0 +1,62 @@
+/*
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+    你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+    示例:
+
+    给定 nums = [2, 7, 11, 15], target = 9
+
+    因为 nums[0] + nums[1] = 2 + 7 = 9
+    所以返回 [0, 1]
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/two-sum
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         for (int i = 0; i < nums.size() - 1; i++) {
+//             for (int j = i + 1; j < nums.size(); j++) {
+//                 if (target - nums.at(i) == nums.at(j)) {
+//                     return vector<int>{i, j};
+//                 }
+//             }
+//         }
+//         return vector<int>();
+//     }
+// };
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        map<int, int> tmpMap;
+        for (int i=0; i<nums.size(); i++) {
+            tmpMap[nums.at(i)] = i;
+        }
+
+        for (int i=0; i<nums.size(); i++) {
+            int compare = target - nums.at(i);
+            if (tmpMap[compare] > 0 && tmpMap[compare] != i) {
+                cout << i << "," << tmpMap[compare];
+                return vector<int>{i, tmpMap[compare]};
+            }
+        }
+        return vector<int>();
+    }
+};
+
+int main() {
+    Solution slut;
+    vector<int> arrary = {1, 2, 3, 4, 5, 6};
+    slut.twoSum(arrary, 4);
+    return 0;
+}
